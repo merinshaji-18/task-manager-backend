9d774eb3a779f8f969249daed87a00b627c41a8a
merinshaji-18 <merin.shaji.10e@gmail.com>
2026-06-18 10:56:39 +0530

    pdf upload

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.database.connection import Base
@@ -15,4 +15,18 @@ class Task(Base):
     due_date = Column(DateTime, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     owner_id = Column(Integer, ForeignKey("users.id"))# Link to User
+    
     owner = relationship("User", back_populates="tasks") 
+    attachments = relationship("Attachment", back_populates="task", cascade="all, delete-orphan")
+
+class Attachment(Base):
+    __tablename__ = "attachments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    task_id = Column(Integer, ForeignKey("tasks.id", ondelete="CASCADE"))
+    file_url = Column(Text, nullable=False) # Cloudinary Link
+    file_name = Column(String, nullable=False) # e.g. "Logo_Final.pdf"
+    file_type = Column(String) # e.g. "image/png" or "application/pdf"
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    task = relationship("Task", back_populates="attachments")
