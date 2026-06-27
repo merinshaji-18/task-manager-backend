a7ab3c913262cd5a4d647e0aa2b57bc53479e5c3
merinshaji-18 <merin.shaji.10e@gmail.com>
2026-06-27 11:45:08 +0530

    -

--- app/core/scheduler.py
+++ app/core/scheduler.py
@@ -8,21 +8,6 @@ import smtplib
 from email.mime.text import MIMEText
 from app.core.config import settings
 
-def send_deadline_email(user_email, task_title, due_time):
-    sender_email = "taskmanagerworkspace@gmail.com"
-    msg = MIMEText(f"CRITICAL ALERT: Your objective '{task_title}' is reaching its deadline at {due_time}. Please ensure all parameters are met.")
-    msg['Subject'] = f"⚠️ DEADLINE ALERT: {task_title}"
-    msg['From'] = f"Workspace <{sender_email}>"
-    msg['To'] = user_email
-
-    try:
-        with smtplib.SMTP("smtp-relay.brevo.com", 2525) as server:
-            server.starttls()
-            server.login("ae70ae001@smtp-brevo.com", settings.BREVO_API_KEY)
-            server.send_message(msg)
-            print("EMAIL FUNCTION CALLED")
-    except Exception as e:
-        print(f"Notification Error: {e}")
 
 def check_deadlines():
     db: Session = SessionLocal()
@@ -59,10 +44,8 @@ def check_deadlines():
         for task in upcoming_tasks:
             user = db.query(User).filter(User.id == task.owner_id).first()
             if user:
-                send_deadline_email(user.email, task.title, task.due_date.strftime("%d-%m-%Y %H:%M"))
                 task.notification_sent = True
                 db.commit()
-                print(f"SUCCESS: Notification dispatched to {user.email}")
     finally:
         db.close()
 
